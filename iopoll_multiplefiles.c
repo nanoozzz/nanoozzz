f7cd6bb6c6f286e06894bb04e94bc5824ec3aebe
Nguyen Le <153147286+nanoozzz@users.noreply.github.com>
2024-02-05 16:29:37 +0700

    Update iopoll_multiplefiles.c

--- iopoll_multiplefiles.c
+++ iopoll_multiplefiles.c
@@ -91,36 +91,25 @@ int main(int argc, char *argv[])
 		if (ret < 0){
 			printf("Error polling\n");
 		}
-		printf("Checkpoint 1\n");
 		
 		if (fds[0].revents & POLLIN) {
-			printf("Checkpoint 2a\n");
 			write(fds[0].fd, &irq0, sizeof(int));
-			printf("Checkpoint 3a\n");
 			res0 = read(fd0, &buf0, sizeof(int));
-			printf("Checkpoint 4a\n");
 			if (res0 > 0) {
 				printf("Interrupt uio0 caught\n");
-				//*((uint32_t *)(gpios0 + GPIO2_DATA_OFFSET / sizeof(uint32_t))) = *((uint32_t *)(gpios0 + GPIO_DATA_OFFSET / sizeof(uint32_t)));
 				tmp0 = *((uint32_t *)(gpios0 + GPIO_DATA_OFFSET / sizeof(uint32_t)));
 			}
 			*((uint32_t *)(gpios0 + IP_ISR / sizeof(uint32_t))) = 0x1; // toggle IP_ISR
-			printf("Checkpoint 5a\n");
 		}
 
 		if (fds[1].revents & POLLIN) {
-			printf("Checkpoint 2b\n");
 			write(fds[1].fd, &irq1, sizeof(int));
-			printf("Checkpoint 3b\n");
                         res1 = read(fd1, &buf1, sizeof(int));
-			printf("Checkpoint 4b\n");
                         if (res1 > 0) {
                                 printf("Interrupt uio1 caught\n");
 				tmp1 = *((uint32_t *)(gpios1 + GPIO_DATA_OFFSET / sizeof(uint32_t))) << 2;
-                        	//*((uint32_t *)(gpios0 + GPIO2_DATA_OFFSET / sizeof(uint32_t))) = *((uint32_t *)(gpios1 + GPIO_DATA_OFFSET / sizeof(uint32_t))) << 2;
 			}
 			*((uint32_t *)(gpios1 + IP_ISR / sizeof(uint32_t))) = 0x1; // toggle IP_ISR
-                	printf("Checkpoint 5b\n");
 		}
 		*((uint32_t *)(gpios0 + GPIO2_DATA_OFFSET / sizeof(uint32_t))) = tmp0 | tmp1;
 	}
